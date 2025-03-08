c459fdec74aef152382c1c0716821a8a0e00746b
AliFareed0009 <alifareed0009@gmail.com>
2025-03-08 16:07:22 +0500

    Adding binary search program

--- binary_search_simple.py
+++ binary_search_simple.py
@@ -1,22 +1,20 @@
-def binary_search(l, target, low=None, high=None):
-    if low is None:
-        low = 0
-    if high is None:
-        high = len(l) - 1
-    if high < low:
-        return -1
+def binary_search(l, target):
+    left,right = 0, len(l) - 1  #left = 0 , right len(l) - 1
 
-    midpoint = (low + high) // 2 
+    while (left <= right):
+        midpoint = (left + right) // 2 
 
-    if l[midpoint] == target:
-        return midpoint
-    elif target < l[midpoint]:
-        return binary_search(l, target, low, midpoint-1)
-    else:
-        return binary_search(l, target, midpoint+1, high)
+        if l[midpoint] == target:
+            return midpoint
+        elif l[midpoint] < target:
+            left = midpoint + 1
+        else:
+            right = midpoint - 1
+
+    return "Target not found"
 
 if __name__=='__main__':
 
     l = [1, 3, 5, 10, 12]
-    target = 5
+    target = 10
     print(binary_search(l, target))
